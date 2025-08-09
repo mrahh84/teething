dcab3617f04479b761c3a3db3b905a715af78cf2
mrahh84 <164894825+mrahh84@users.noreply.github.com>
2025-08-09 16:45:51 -0400

    Fix main_security AttributeError: remove common/views/ package directory that was shadowing common/views.py module

--- common/views/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Modular views package.
-
-This package progressively replaces the monolithic views.py by grouping views
-into focused modules. During migration, common.views still re-exports certain
-callables to keep URL names and imports stable.
-"""
-
--- common/views/security_views.py
+++
@@ -1,8 +0,0 @@
-"""Security-related views module (Phase 1 modularization)."""
-from .. import views as legacy_views
-
-main_security = legacy_views.main_security
-main_security_clocked_in_status_flip = legacy_views.main_security_clocked_in_status_flip
-main_security_clock_out = legacy_views.main_security_clock_out
-main_security_clock_in = legacy_views.main_security_clock_in
-
